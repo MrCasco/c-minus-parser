38bcdfe6868a4142e182806bdcfb001fe74d58ab
Ernesto Casco <ecasco@fb.com>
2022-06-08 21:14:35 -0500

    Updated cgen.py

--- proyecto/cgen.py
+++ proyecto/cgen.py
@@ -1,20 +1,14 @@
 def suma(t):
+    codeGen(t.childNodes[0], file)
+    fileCode.append('  sw $a0 0($sp)')
+    fileCode.append('  addiu $sp $sp ‐4')
+    codeGen(t.childNodes[1].childNodes[1], file)
+    fileCode.append('  lw $t1 4($sp)')
     if t.op == '-':
-        codeGen(t.childNodes[0], file)
-        fileCode.append('     sw $a0 0($sp)')
-        fileCode.append('     addiu $sp $sp ‐4')
-        codeGen(t.childNodes[1].childNodes[1], file)
-        fileCode.append('     lw $t1 4($sp)')
-        fileCode.append('     sub $a0 $t1 $a0')
-        fileCode.append('     addiu $sp $sp 4')
-    elif t.op == '+':
-        codeGen(t.childNodes[0], file)
-        fileCode.append('     sw $a0 0($sp)')
-        fileCode.append('     addiu $sp $sp ‐4')
-        codeGen(t.childNodes[1].childNodes[1], file)
-        fileCode.append('     lw $t1 4($sp)')
-        fileCode.append('     add $a0 $t1 $a0')
-        fileCode.append('     addiu $sp $sp 4')
+        fileCode.append('  sub $a0 $t1 $a0')
+    else:
+        fileCode.append('  add $a0 $t1 $a0')
+    fileCode.append('  addiu $sp $sp 4')
 
 
 def codeGen(t, file):
@@ -28,5 +22,5 @@ def codeGen(t, file):
 
         elif t.statement == ExpKind.OpK:
             if t.op == '+' or t.op == '-':
-                suma(t, t.op)
-    else:
+                suma(t)
+        
